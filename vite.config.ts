74ee9547ea0b02811c50f6e6da06cd7a2d63565a
benjaminleonard <benji@oxide.computer>
2026-06-03 10:25:26 +0100

    Remove old code

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,4 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: { port: 3000 },
-  optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg'],
-  },
 })
